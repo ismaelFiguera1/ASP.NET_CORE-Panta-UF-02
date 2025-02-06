0aeb17f02618b1d243e8a8862051ceb0dc78a6c6
ismaelFiguera1 <ismaelfiguera112@gmail.com>
2025-02-07 00:14:33 +0100

    fet1.0

--- Tasca1/Tasca1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Tasca1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
