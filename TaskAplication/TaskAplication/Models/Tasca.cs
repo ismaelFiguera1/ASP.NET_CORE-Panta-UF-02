09a2202b6d7e3b16607c3e87d4e12dab37712e2e
ismaelFiguera1 <ismaelfiguera112@gmail.com>
2025-05-30 14:56:21 +0200

    practica

---
+++ TaskAplication/TaskAplication/Models/Tasca.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskAplication.Models
+{
+    public class Tasca
+    {
+        public string Description { get; set; }
+
+        public DateTime CrearModificar { get; set; }
+
+        public bool TascaFeta { get; set; }
+
+        public static int Contador { get; set; } = 0;
+
+        public int Id { get; set; }
+
+        public Tasca(string des)
+        {
+            this.Description = des;
+            this.CrearModificar = DateTime.Now;
+            this.TascaFeta = false;
+            Contador = Contador + 1;
+            this.Id = Contador;
+        }
+
+        public Tasca()
+        {
+            this.CrearModificar = DateTime.Now;
+            this.TascaFeta = false;
+            Contador = Contador + 1;
+            this.Id = Contador;
+        }
+    }
+}
